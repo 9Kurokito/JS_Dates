8e535f8ae25543e7528b0a04639f59bd5e000632
Roman <roman.sazanoff2012@yandex.ru>
2024-03-29 13:06:25 +0300

    Задача 3 решена

--- js/dates.js
+++ js/dates.js
@@ -1,7 +1,7 @@
 function task1(){
     let getDayOfWeek = (data) => daysOfWeek[data.getDay()];
     let data = new Date();
-    let daysOfWeek= [
+    let daysOfWeek = [
         'Воскресенье',
         'Понедельник',
         'Вторник',
@@ -19,4 +19,21 @@ function task2(){
     dataNewYear.setFullYear(dataCurrent.getFullYear() + 1);
     let diff = (dataNewYear - dataCurrent) / (1000 * 60 * 60 * 24);
     console.log(diff.toFixed(0));
+}
+
+function task3(){
+    let getDayOfWeek = (data) => daysOfWeek[data.getDay()];
+    let getPrevDay = (data) => daysOfWeek[data.getDay() - 1];
+    let getNextday = (data) => daysOfWeek[data.getDay() + 1];
+    let data = new Date();
+    let daysOfWeek = [
+        'Воскресенье',
+        'Понедельник',
+        'Вторник',
+        'Среда',
+        'Четверг',
+        'Пятница',
+        'Суббота' 
+    ];
+    alert(`next: ${getNextday(data)}, curr: ${getDayOfWeek(data)}, prev: ${getPrevDay(data)}`);
 }
